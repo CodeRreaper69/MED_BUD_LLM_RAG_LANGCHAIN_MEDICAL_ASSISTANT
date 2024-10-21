c1a22c9e8d908356bcdc03b32a2f9706e898d885
Sourabh Dey <deysourabh8981@gmail.com>
2024-10-21 13:19:26 +0530

    Update 2 🌐 Medi_Bud_With_Web_Access.py

--- pages/2 🌐 Medi_Bud_With_Web_Access.py
+++ pages/2 🌐 Medi_Bud_With_Web_Access.py
@@ -45,20 +45,23 @@ class InternetChatbot:
     def main(self):
         agent_executor, memory = self.setup_agent()
         user_query = st.chat_input(placeholder="Ask me anything!")
-        if user_query:
-            utils.display_msg(user_query, 'user')
-            with st.chat_message("assistant"):
-                st_cb = StreamlitCallbackHandler(st.container())
-                result = agent_executor.invoke(
-                    {"input": user_query, "chat_history": memory.chat_memory.messages},
-                    {"callbacks": [st_cb]}
-                )
-                response = result["output"]
-                st.session_state.messages.append({"role": "assistant", "content": response})
-                st.write(response)
-                utils.print_qa(InternetChatbot, user_query, response)
-
-
+        try:
+            if user_query:
+                utils.display_msg(user_query, 'user')
+                with st.chat_message("assistant"):
+                    st_cb = StreamlitCallbackHandler(st.container())
+                    result = agent_executor.invoke(
+                        {"input": user_query, "chat_history": memory.chat_memory.messages},
+                        {"callbacks": [st_cb]}
+                    )
+                    response = result["output"]
+                    st.session_state.messages.append({"role": "assistant", "content": response})
+                    st.write(response)
+                    utils.print_qa(InternetChatbot, user_query, response)
+        except:
+            st.warning("Some error occured due to large requests fetching... please try something less complex or simpler for the model to react")
+            #utils.print_qa(InternetChatbot, user_query, response)
+                
 if __name__ == "__main__":
     obj = InternetChatbot()
     obj.main()

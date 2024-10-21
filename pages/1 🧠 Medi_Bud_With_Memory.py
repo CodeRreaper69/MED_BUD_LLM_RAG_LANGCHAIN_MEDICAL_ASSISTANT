b9e5688c13382876c48b6ecff198d8fa305b26f5
Sourabh Dey <deysourabh8981@gmail.com>
2024-10-21 13:49:02 +0530

    Update 1 🧠 Medi_Bud_With_Memory.py

--- pages/1 🧠 Medi_Bud_With_Memory.py
+++ pages/1 🧠 Medi_Bud_With_Memory.py
@@ -142,7 +142,7 @@ class MedicalChatbot:
                     if "emergency" in user_query.lower() or "urgent" in user_query.lower():
                         st.warning("⚠️ Based on your query, we recommend seeking immediate medical assistance.")
         except:
-            st.warning("Some error might have happened due to the size of the files")
+            st.warning("Some error might have happened due to the size of the files, reduce the file size and try again")
 
 if __name__ == "__main__":
     obj = MedicalChatbot()
